41abd78b5101cb65b2464c406b0c71a4bcfe9fbb
Vadim Piskarev <woxadestroyer@gmail.com>
2022-07-01 19:34:22 +0300

    build was fixed

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -15,6 +15,44 @@ public class RadioTest {
     }
 
     @Test
+    public void shouldNotSetRadioStationAboveMax() {
+        Radio radio = new Radio(30);
+
+        radio.setCurrentRadioStationNumber(50);
+
+        Assertions.assertEquals(0, radio.getCurrentRadioStationNumber());
+    }
+
+    @Test
+    public void shouldNotSetRadioStationBelowMin() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStationNumber(-2);
+
+        Assertions.assertEquals(0, radio.getCurrentRadioStationNumber());
+    }
+
+    @Test
+    public void shouldSetNextRadioStation() {
+        Radio radio = new Radio(20);
+
+        radio.setCurrentRadioStationNumber(5);
+        radio.next();
+
+        Assertions.assertEquals(6, radio.getCurrentRadioStationNumber());
+    }
+
+    @Test
+    public void shouldSetPrevRadioStation() {
+        Radio radio = new Radio(20);
+
+        radio.setCurrentRadioStationNumber(5);
+        radio.prev();
+
+        Assertions.assertEquals(4, radio.getCurrentRadioStationNumber());
+    }
+
+    @Test
     public void shouldNotSetNextRadioStationAboveMax() {
         Radio radio = new Radio(20);
 
@@ -42,20 +80,55 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldNotSetRadioStationAboveMax() {
+    public void shouldNotSetSoundVolumeAboveMax() {
 
-        radio.setCurrentRadioStationNumber(100);
-        radio.increase();
+        radio.setSoundVolume(120);
 
-        Assertions.assertEquals(0, radio.getCurrentRadioStationNumber());
+        Assertions.assertEquals(0, radio.getSoundVolume());
     }
 
     @Test
-    public void shouldNotSetRadioStationBelowMin() {
+    public void shouldNotSetSoundVolumeBelowMin() {
 
-        radio.setCurrentRadioStationNumber(0);
+        radio.setSoundVolume(-10);
+
+        Assertions.assertEquals(0, radio.getSoundVolume());
+    }
+
+    @Test
+    public void shouldIncreaseSoundVolume() {
+
+        radio.setSoundVolume(10);
+        radio.increase();
+
+        Assertions.assertEquals(11, radio.getSoundVolume());
+    }
+
+    @Test
+    public void shouldDecreaseSoundVolume() {
+
+        radio.setSoundVolume(10);
         radio.decrease();
 
-        Assertions.assertEquals(0, radio.getCurrentRadioStationNumber());
+        Assertions.assertEquals(9, radio.getSoundVolume());
+    }
+
+
+    @Test
+    public void shouldNotIncreaseSoundVolumeAboveMax() {
+
+        radio.setSoundVolume(100);
+        radio.increase();
+
+        Assertions.assertEquals(100, radio.getSoundVolume());
+    }
+
+    @Test
+    public void shouldNotDecreaseSoundVolumeBelowMin() {
+
+        radio.setSoundVolume(0);
+        radio.decrease();
+
+        Assertions.assertEquals(0, radio.getSoundVolume());
     }
 }
